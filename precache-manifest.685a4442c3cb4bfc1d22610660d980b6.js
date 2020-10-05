582bc9c536b9648a12cf0af3db79b261d2510a22
Jagmohan Mandre <jagmohan.mandre@leptonmaps.com>
2020-10-06 02:01:29 +0530

    Updates

---
+++ precache-manifest.685a4442c3cb4bfc1d22610660d980b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75cb33b591b90202d739969819953f39",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "0892948ad6e4a546b047",
+    "url": "/portfolio-app/static/css/2.657920fb.chunk.css"
+  },
+  {
+    "revision": "6b6aa291ae170cf3b2cb",
+    "url": "/portfolio-app/static/css/main.e711a051.chunk.css"
+  },
+  {
+    "revision": "0892948ad6e4a546b047",
+    "url": "/portfolio-app/static/js/2.ee65660d.chunk.js"
+  },
+  {
+    "revision": "6b6aa291ae170cf3b2cb",
+    "url": "/portfolio-app/static/js/main.dae91cf6.chunk.js"
+  },
+  {
+    "revision": "44340542c48e10c20cd6",
+    "url": "/portfolio-app/static/js/runtime~main.cb97d77a.js"
+  }
+]);
